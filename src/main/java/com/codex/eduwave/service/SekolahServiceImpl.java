c4cb397218495e756a18a3d5d3ec158c4756c699
ahmadelham21 <ahmadelham21@gmail.com>
2024-07-25 23:29:29 +0700

    [ADD] adding ImageService and SekolahService

--- src/main/java/com/codex/eduwave/service/SekolahServiceImpl.java
+++ src/main/java/com/codex/eduwave/service/SekolahServiceImpl.java
@@ -3,28 +3,44 @@ package com.codex.eduwave.service;
 import com.codex.eduwave.entity.Sekolah;
 import com.codex.eduwave.model.request.SekolahRequest;
 import com.codex.eduwave.repository.SekolahRepository;
+import com.codex.eduwave.service.intrface.ImageService;
 import com.codex.eduwave.service.intrface.SekolahService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.Date;
 
 @Service
 @RequiredArgsConstructor
 public class SekolahServiceImpl implements SekolahService {
 
     private final SekolahRepository sekolahRepository;
-//    @Override
-//    public Sekolah createSekolah(Sekolah request) {
-//        return sekolahRepository.saveAndFlush(request);
-//    }
+    private final ImageService imageService;
 
     @Override
     public Sekolah createSekolah(SekolahRequest request) {
+        String logoUrl = imageService.create(request.getLogo());
 
-        return null;
+        Sekolah sekolah = Sekolah.builder()
+                .createdAt(new Date())
+                .updatedAt(new Date())
+                .sekolah(request.getSekolah())
+                .email(request.getEmail())
+                .noHp(request.getNoHp())
+                .npsn(request.getNpsn())
+                .logo(logoUrl)
+                .isDeleted(false)
+                .build();
+      return sekolahRepository.saveAndFlush(sekolah);
+
+
+
     }
 
     @Override
     public Sekolah getById(String id) {
-        return null;
+        return sekolahRepository.findById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Data not found"));
     }
 }
