fdde730a5db41a52245453edf5e43271f75ae7bb
ahmadelham21 <ahmadelham21@gmail.com>
2024-07-25 20:30:22 +0700

    [ADD] adding repository

---
+++ src/main/java/com/codex/eduwave/repository/SiswaRepository.java
@@ -0,0 +1,7 @@
+package com.codex.eduwave.repository;
+
+import com.codex.eduwave.entity.Siswa;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SiswaRepository extends JpaRepository<Siswa, String> {
+}
