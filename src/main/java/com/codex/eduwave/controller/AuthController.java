551f03a5eac8dd3bc2f940cd44fab5a1c1e879b7
adityae <rifkyaditya966@gmail.com>
2024-07-26 16:55:30 +0700

    [ADD] remove trace

--- src/main/java/com/codex/eduwave/controller/AuthController.java
+++ src/main/java/com/codex/eduwave/controller/AuthController.java
@@ -2,9 +2,12 @@ package com.codex.eduwave.controller;
 
 import com.codex.eduwave.constant.ApiUrl;
 import com.codex.eduwave.model.request.AuthRequest;
+import com.codex.eduwave.model.response.BaseResponse;
+import com.codex.eduwave.model.response.CommonResponse;
 import com.codex.eduwave.model.response.LoginResponse;
 import com.codex.eduwave.service.intrface.AuthService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -19,8 +22,14 @@ public class AuthController {
     private final AuthService authService;
 
     @PostMapping(path = "/login")
-    public ResponseEntity<LoginResponse> login(@RequestBody AuthRequest request) {
-        LoginResponse response = authService.login(request);
+    public ResponseEntity<BaseResponse> login(@RequestBody AuthRequest request) {
+        LoginResponse account = authService.login(request);
+        CommonResponse<LoginResponse> response = CommonResponse.<LoginResponse>builder()
+                .statusCode(HttpStatus.ACCEPTED.value())
+                .message("You have successfully logged in")
+                .data(account)
+                .build();
+
 
         return ResponseEntity.ok(response);
     }
