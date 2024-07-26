86b47a4036762a2dd2cffd8339aa2eade9fb1fb5
ahmadelham21 <ahmadelham21@gmail.com>
2024-07-26 17:58:12 +0700

    [UPDATE] filterchain for sekolah api

--- src/main/java/com/codex/eduwave/security/SecurityConfiguration.java
+++ src/main/java/com/codex/eduwave/security/SecurityConfiguration.java
@@ -25,7 +25,7 @@ public class SecurityConfiguration {
                 .authorizeHttpRequests(request ->  request
                         .dispatcherTypeMatchers(DispatcherType.ERROR).permitAll()
                         .requestMatchers("/api/v1/auth/**").permitAll()
-                        .requestMatchers("/api/v1/sekolah/**").permitAll()
+//                        .requestMatchers("/api/v1/sekolah/**").permitAll()
                         .anyRequest().authenticated()
                 )
                 .addFilterBefore(
