c1ede7a282d4054fdf7b7bee02d23212af44bcc2
adityae <rifkyaditya966@gmail.com>
2024-07-26 15:18:33 +0700

    [FIX] auth service and add authentication manager to bean configuration

--- src/main/java/com/codex/eduwave/service/AuthServiceImpl.java
+++ src/main/java/com/codex/eduwave/service/AuthServiceImpl.java
@@ -8,10 +8,15 @@ import com.codex.eduwave.model.response.LoginResponse;
 import com.codex.eduwave.model.response.RegisterResponse;
 import com.codex.eduwave.repository.AccountRepository;
 import com.codex.eduwave.service.intrface.AuthService;
+import com.codex.eduwave.service.intrface.JwtService;
 import com.codex.eduwave.service.intrface.RoleService;
 import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -26,6 +31,10 @@ public class AuthServiceImpl implements AuthService {
     private final AccountRepository accountRepository;
     private final RoleService roleService;
     private final PasswordEncoder passwordEncoder;
+
+    private final AuthenticationManager authenticationManager;
+
+    private final JwtService jwtService;
 
     @Value("${app.eduwave.username-admin}")
     private String AdminUsername;
@@ -55,12 +64,39 @@ public class AuthServiceImpl implements AuthService {
     }
 
     @Override
-    public RegisterResponse register(AuthRequest request) {
-        return null;
+    public Account register(AuthRequest request) {
+        Role role = roleService.getOrSave(UserRole.ROLE_SEKOLAH);
+
+        String hasPassword = passwordEncoder.encode(request.getPassword());
+
+        Account account = Account.builder()
+                .username(request.getUsername())
+                .password(hasPassword)
+                .role(List.of(role))
+                .isEnable(true)
+                .build();
+
+        accountRepository.saveAndFlush(account);
+        return account;
     }
 
     @Override
     public LoginResponse login(AuthRequest request) {
-        return null;
+        Authentication authentication = new UsernamePasswordAuthenticationToken(
+                request.getUsername(),
+                request.getPassword()
+        );
+
+        Authentication authenticated = authenticationManager.authenticate(authentication);
+
+        Account account = (Account) authenticated.getPrincipal();
+
+        String token = jwtService.generateToken(account);
+
+        return LoginResponse.builder()
+                .token(token)
+                .username(account.getUsername())
+                .roles(account.getAuthorities().stream().map(GrantedAuthority::getAuthority).toList())
+                .build();
     }
 }
--- src/main/java/com/codex/eduwave/service/intrface/AuthService.java
+++ src/main/java/com/codex/eduwave/service/intrface/AuthService.java
@@ -1,10 +1,11 @@
 package com.codex.eduwave.service.intrface;
 
+import com.codex.eduwave.entity.Account;
 import com.codex.eduwave.model.request.AuthRequest;
 import com.codex.eduwave.model.response.LoginResponse;
 import com.codex.eduwave.model.response.RegisterResponse;
 
 public interface AuthService {
-    RegisterResponse register(AuthRequest request);
+    Account register(AuthRequest request);
     LoginResponse login(AuthRequest request);
 }
