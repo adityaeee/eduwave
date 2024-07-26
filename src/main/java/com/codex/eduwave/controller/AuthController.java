2ce62b92a33d35b349472d6d91e31ad4d356af45
adityae <rifkyaditya966@gmail.com>
2024-07-26 16:18:05 +0700

    [MODIFY] auth controller loggin

--- src/main/java/com/codex/eduwave/controller/AuthController.java
+++ src/main/java/com/codex/eduwave/controller/AuthController.java
@@ -1,4 +1,27 @@
 package com.codex.eduwave.controller;
 
+import com.codex.eduwave.constant.ApiUrl;
+import com.codex.eduwave.model.request.AuthRequest;
+import com.codex.eduwave.model.response.LoginResponse;
+import com.codex.eduwave.service.intrface.AuthService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping(path = ApiUrl.AUTH)
 public class AuthController {
+
+    private final AuthService authService;
+
+    @PostMapping(path = "/login")
+    public ResponseEntity<LoginResponse> login(@RequestBody AuthRequest request) {
+        LoginResponse response = authService.login(request);
+
+        return ResponseEntity.ok(response);
+    }
 }
