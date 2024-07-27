008588ce95568dd1409de4180fc9ef97bf8f0289
ahmadelham21 <ahmadelham21@gmail.com>
2024-07-28 02:09:17 +0700

    [ADD] adding validation in SekolahRequest,GolonganRequest,SiswaRequest

--- src/main/java/com/codex/eduwave/model/request/SekolahRequest.java
+++ src/main/java/com/codex/eduwave/model/request/SekolahRequest.java
@@ -2,6 +2,9 @@ package com.codex.eduwave.model.request;
 
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -13,12 +16,18 @@ import org.springframework.web.multipart.MultipartFile;
 @NoArgsConstructor
 @Builder
 public class SekolahRequest {
-    private  String sekolah;
+    @NotBlank(message = "Sekolah is mandatory and cannot be blank")
+    private String sekolah;
 
+    @NotBlank(message = "Email is mandatory and cannot be blank")
+    @Email(message = "Email should be valid")
     private String email;
 
+    @NotBlank(message = "Phone Number is mandatory and cannot be blank")
     private String noHp;
 
+    @Pattern(regexp = "^[0-9]+$",message = "NPSN must containt only numbers")
+    @NotBlank(message = "NPSN is mandatory and cannot be blank")
     private String npsn;
 
     private String password;
--- src/main/java/com/codex/eduwave/model/request/SiswaRequest.java
+++ src/main/java/com/codex/eduwave/model/request/SiswaRequest.java
@@ -1,6 +1,9 @@
 package com.codex.eduwave.model.request;
 
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -11,12 +14,30 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @Builder
 public class SiswaRequest {
+
+    @NotBlank(message = "nama is mandatory and cannot be blank")
     private String nama;
+
+
+    @NotBlank(message = "NIS is mandatory and cannot be blank")
+    @Pattern(regexp = "^[0-9]+$",message = "NIS must containt only numbers")
     private String nis;
+
+    @NotBlank(message = "Email is mandatory and cannot be blank")
+    @Email(message = "Email should be valid")
     private String email;
+
+
+    @NotBlank(message = "No hp is mandatory and cannot be blank")
     private String noHp;
+
+    @NotBlank(message = "No hp ortu is mandatory and cannot be blank")
     private String noHpOrtu;
+
+
     private String alamat;
+
+    @NotBlank(message = "golonganId is mandatory and cannot be blank")
     private String golonganId;
 
 
