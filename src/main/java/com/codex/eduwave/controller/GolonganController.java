efb15324b66ac44bb913441b851c98e196ceacf2
ahmadelham21 <ahmadelham21@gmail.com>
2024-07-28 13:18:09 +0700

    [UPDATE] updating SiswaRepo and GolonganController

--- src/main/java/com/codex/eduwave/controller/GolonganController.java
+++ src/main/java/com/codex/eduwave/controller/GolonganController.java
@@ -3,13 +3,17 @@ package com.codex.eduwave.controller;
 import com.codex.eduwave.constant.ApiUrl;
 import com.codex.eduwave.entity.Golongan;
 import com.codex.eduwave.model.request.GolonganRequest;
+import com.codex.eduwave.model.request.UpdateGolonganRequest;
 import com.codex.eduwave.model.response.BaseResponse;
 import com.codex.eduwave.model.response.CommonResponse;
+import com.codex.eduwave.model.response.CommonResponseWithPage;
 import com.codex.eduwave.service.intrface.GolonganService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -45,5 +49,45 @@ public class GolonganController {
 
         return ResponseEntity.ok(response);
 
+    }
+
+    @GetMapping
+    public ResponseEntity<BaseResponse> getAll(){
+        List<Golongan> golonganList = golonganService.getAll();
+
+        BaseResponse response = CommonResponseWithPage.builder()
+                .statusCode(HttpStatus.OK.value())
+                .message("successfully get data")
+                .data(golonganList)
+                .build();
+        return ResponseEntity.ok(response);
+    }
+
+    @PutMapping
+    public ResponseEntity<BaseResponse> update(
+            @RequestBody UpdateGolonganRequest request
+            ){
+
+        Golongan golongan = golonganService.update(request);
+
+        BaseResponse response = CommonResponse.builder()
+                .statusCode(HttpStatus.OK.value())
+                .message("successfully update data")
+                .data(golongan)
+                .build();
+        return ResponseEntity.ok(response);
+    }
+
+    @DeleteMapping(path = ApiUrl.PATH_VAR_ID)
+    public ResponseEntity<BaseResponse> update(
+            @PathVariable String id
+    ){
+        golonganService.delete(id);
+
+        BaseResponse response = CommonResponse.builder()
+                .statusCode(HttpStatus.OK.value())
+                .message("successfully delete data")
+                .build();
+        return ResponseEntity.ok(response);
     }
 }
--- src/main/java/com/codex/eduwave/repository/SiswaRepository.java
+++ src/main/java/com/codex/eduwave/repository/SiswaRepository.java
@@ -7,5 +7,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface SiswaRepository extends JpaRepository<Siswa, String> {
-    List<Siswa> findByIsDeletedFalse();
+
 }
