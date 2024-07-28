c7fdd490574fb5974eb21ef3d5523c1bd1e9513b
ahmadelham21 <ahmadelham21@gmail.com>
2024-07-28 13:01:58 +0700

    [ADD] adding getAll,getByIdIfExist, delete, update in GolonganService

--- src/main/java/com/codex/eduwave/service/GolonganServiceImpl.java
+++ src/main/java/com/codex/eduwave/service/GolonganServiceImpl.java
@@ -3,6 +3,7 @@ package com.codex.eduwave.service;
 import com.codex.eduwave.entity.Golongan;
 import com.codex.eduwave.entity.Sekolah;
 import com.codex.eduwave.model.request.GolonganRequest;
+import com.codex.eduwave.model.request.UpdateGolonganRequest;
 import com.codex.eduwave.model.response.SekolahResponse;
 import com.codex.eduwave.repository.GolonganRepository;
 import com.codex.eduwave.service.intrface.GolonganService;
@@ -14,6 +15,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.util.Date;
+import java.util.List;
 
 
 @Service
@@ -47,4 +49,48 @@ public class GolonganServiceImpl implements GolonganService {
     public Golongan getById(String id) {
         return golonganRepository.findById(id).orElseThrow(()-> new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR,"data not found"));
     }
+
+    @Override
+    public List<Golongan> getAll() {
+        List<Golongan> listGolongan = golonganRepository.findByIsDeletedFalse();
+        return listGolongan;
+    }
+
+    @Override
+    public Golongan update(UpdateGolonganRequest request) {
+        Golongan golongan = getByIdIfExist(request.getId());
+
+        return golonganRepository.saveAndFlush(Golongan.builder()
+                .id(golongan.getId())
+                .golongan(request.getGolongan())
+                .spp(request.getSpp())
+                .sekolah(golongan.getSekolah())
+                .isDeleted(golongan.getIsDeleted())
+                .createdAt(golongan.getCreatedAt())
+                .updatedAt(new Date())
+                .build());
+    }
+
+    @Override
+    public void delete(String id) {
+        Golongan golongan = getByIdIfExist(id);
+        golongan.setIsDeleted(true);
+
+        golonganRepository.saveAndFlush(golongan);
+
+
+    }
+
+
+
+    private Golongan getByIdIfExist(String id){
+        Golongan golongan = golonganRepository.findById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Data not found"));
+
+        if (golongan.getIsDeleted()){
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Data not found");
+        }
+    return golongan;
+    }
+
+
 }
